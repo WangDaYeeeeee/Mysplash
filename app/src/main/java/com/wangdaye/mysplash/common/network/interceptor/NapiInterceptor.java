2a1d8b2c10c4dfee2fcd0333172c89c6089170bd
WangDaYeeeeee <wangdayeeeeee@gmail.com>
2019-04-10 07:15:57 +0800

    3.6.7

--- app/src/main/java/com/wangdaye/mysplash/common/network/interceptor/NapiInterceptor.java
+++ app/src/main/java/com/wangdaye/mysplash/common/network/interceptor/NapiInterceptor.java
@@ -47,42 +47,6 @@ public class NapiInterceptor extends ReportExceptionInterceptor {
     }
 
     private Response buildResponse(Request request, Response response) throws IOException {
-        // return response;
-
-        ResponseBody body = response.body();
-        if (body == null) {
-            return response;
-        }
-
-        BufferedSource source = body.source();
-        source.request(Long.MAX_VALUE); // Buffer the entire body.
-        Buffer buffer = source.buffer();
-
-        if ("gzip".equalsIgnoreCase(response.headers().get("Content-Encoding"))) {
-            try (GzipSource gzippedResponseBody = new GzipSource(buffer.clone())) {
-                buffer = new Buffer();
-                buffer.writeAll(gzippedResponseBody);
-            }
-        }
-
-        Charset charset = UTF8;
-        MediaType contentType = body.contentType();
-        if (contentType != null) {
-            charset = contentType.charset(UTF8);
-        }
-
-        String bodyString = "";
-        if (charset != null) {
-            bodyString = buffer.clone().readString(charset);
-        }
-
-        return new Response.Builder()
-                .addHeader("Content-Type", "application/json")
-                .code(response.code())
-                .body(ResponseBody.create(body.contentType(), bodyString))
-                .message(response.message())
-                .request(request)
-                .protocol(Protocol.HTTP_2)
-                .build();
+        return response;
     }
 }
