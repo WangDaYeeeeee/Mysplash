978a16b29700627aee71858eba722bc46ec6c505
WangDaYeeeeee <wangdayeeeeee@gmail.com>
2019-10-06 10:09:42 +0800

    3.7.7

--- common/src/main/java/com/wangdaye/common/utils/helper/CrashReportHelper.java
+++ common/src/main/java/com/wangdaye/common/utils/helper/CrashReportHelper.java
@@ -2,7 +2,7 @@ package com.wangdaye.common.utils.helper;
 
 import android.content.Context;
 
-import com.tencent.bugly.crashreport.CrashReport;
+// import com.tencent.bugly.crashreport.CrashReport;
 
 /**
  * Crash report helper.
@@ -10,10 +10,10 @@ import com.tencent.bugly.crashreport.CrashReport;
 public class CrashReportHelper {
 
     public static void init(Context context) {
-        CrashReport.initCrashReport(context.getApplicationContext(), "c8ad99bd5d", false);
+        // CrashReport.initCrashReport(context.getApplicationContext(), "c8ad99bd5d", false);
     }
 
     public static void report(Throwable t) {
-        CrashReport.postCatchedException(t);
+        // CrashReport.postCatchedException(t);
     }
 }
